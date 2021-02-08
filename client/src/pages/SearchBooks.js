927925d0f6b149facf80c8acb507babe74ef02f4
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2021-02-07 22:11:16 -0500

    fixing error in web address for fetch in searchbooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -33,7 +33,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await fetch(`https://www.googleapis/com/books/v1/volumes?q=${searchInput}`);
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new error('something went wrong!');
